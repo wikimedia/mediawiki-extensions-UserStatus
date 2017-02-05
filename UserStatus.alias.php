b0d42bd4407280717ed6a773fc199b0b0101fc50
Umherirrender <umherirrender_de.wp@web.de>
2017-02-05 15:17:47 +0100

    Add file for special page aliases
    Allows translations and avoids:
    Notice: Did not find alias for special page '<SomeSpecialPageName>'.
    Perhaps no aliases are defined for it?
    
    Change-Id: I250098237c5b59fe6995d7d27c78c4d4701dbe03

---
+++ UserStatus.alias.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Aliases for special pages
+ *
+ * @file
+ * @ingroup Extensions
+ */
+// @codingStandardsIgnoreFile
+
+$specialPageAliases = array();
+
+/** English (English) */
+$specialPageAliases['en'] = array(
+	'FanUpdates' => array( 'FanUpdates' ),
+	'UserStatus' => array( 'UserStatus' ),
+	'ViewThought' => array( 'ViewThought' ),
+);
