5891dac6a30d198cd1da8982f9fd8ca5ebff3a8f
Antoine Musso <hashar@free.fr>
2017-04-24 12:12:25 +0200

    Build: add npm test command
    Change-Id: Ic2eff99e883cf080f1067885757a1fdc79f26314
    Depends-On: Ie9c406ce191dbbc1100436490dbd9009de01899e

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+/* eslint-env node */
+module.exports = function ( grunt ) {
+	var conf = grunt.file.readJSON( 'extension.json' );
+
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+
+	grunt.initConfig( {
+		jshint: {
+			all: [
+				'**/*.js',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		},
+		banana: conf.MessagesDirs,
+		jsonlint: {
+			all: [
+				'**/*.json',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'jshint', 'jsonlint', /** 'banana' **/] );
+	grunt.registerTask( 'default', 'test' );
+};
